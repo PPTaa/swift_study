f63c6dcccd632a37b626e7303aca8100a6c63956
pptaa <chobo05@naver.com>
2020-12-02 13:51:04 +0900

    Initial Commit

---
+++ StartSwiftUI/StartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartSwiftUIApp.swift
+//  StartSwiftUI
+//
+//  Created by 맥북에어 on 2020/12/02.
+//
+
+import SwiftUI
+
+@main
+struct StartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
